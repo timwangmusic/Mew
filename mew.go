0e26e314180878fdbe06b3cc3ab2b52984c9866b
Tim Wang <weihewang2012@gmail.com>
2020-04-04 14:58:12 -0700

    refactor market buy/sell commands

--- mew.go
+++ mew.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"flag"
-	"github.com/weihesdlegend/Mew/transactions"
 	"os"
 	"strings"
 
 	"astuart.co/go-robinhood"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"github.com/urfave/cli/v2"
 	"github.com/weihesdlegend/Mew/config"
+	"github.com/weihesdlegend/Mew/transactions"
 )
 
 // example input from CLI: mew buy -s 100 -t AAPL
 // output: purchased 100 shares of AAPL with market order, and total cost is xxx
 func main() {
 	log.Info("welcome to use the Mew stock assistant")
-
-	if len(os.Args) < 2 {
-		log.Info("no transaction type specified")
-		os.Exit(1)
-	}
 
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
@@ -36,7 +31,7 @@ func main() {
 		log.Fatal("Unable to decode into struct, %v", err)
 	}
 
-	cli, err := robinhood.Dial(&robinhood.OAuth{
+	rhClient, err := robinhood.Dial(&robinhood.OAuth{
 		Username: cfg.Broker.User,
 		Password: cfg.Broker.Password,
 	})
@@ -45,48 +40,73 @@ func main() {
 		log.Fatal("Robinhood auth error %s", err)
 	}
 
-	// example of a simple market order buy
-	buyCmd := flag.NewFlagSet("buy", flag.ExitOnError)
-	sellCmd := flag.NewFlagSet("sell", flag.ExitOnError)
+	app := cli.NewApp()
 
 	var ticker string
-	buyCmd.StringVar(&ticker, "t", "YANG", "stock ticker")
-	sellCmd.StringVar(&ticker, "t", "YANG", "stock ticker")
+	var shares uint64
 
-	var numShares uint64
-	buyCmd.Uint64Var(&numShares, "n", 0, "number of shares to purchase")
-	sellCmd.Uint64Var(&numShares, "n", 0, "number of shares to purchase")
-
-	var orderType string
-	buyCmd.StringVar(&orderType, "o", "market", "order type")
-	sellCmd.StringVar(&orderType, "o", "market", "order type")
-
-	switch os.Args[1] {
-	case "buy":
-		_ = buyCmd.Parse(os.Args[2:])
-	case "sell":
-		_ = sellCmd.Parse(os.Args[2:])
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
+	tickerFlag := cli.StringFlag{
+		Name:        "ticker",
+		Aliases:     []string{"t"},
+		Value:       "YANG",
+		Required:    false,
+		Destination: &ticker,
 	}
 
-	ticker = strings.ToUpper(ticker)
-	orderType = strings.ToLower(orderType)
+	sharesFlag := cli.Uint64Flag{
+		Name:        "shares",
+		Aliases:     []string{"s"},
+		Value:       0,
+		Required:    false,
+		Destination: &shares,
+	}
 
-	if buyCmd.Parsed(){
-		buyErr := transactions.PlaceMarketOrder(cli, ticker, numShares, robinhood.Buy)
-		if buyErr != nil {
-			log.Error(buyErr)
-		} else {
-			log.Infof("purchased %d shares of %s with %s order", numShares, ticker, orderType)
-		}
-	} else if sellCmd.Parsed() {
-		sellErr := transactions.PlaceMarketOrder(cli, ticker, numShares, robinhood.Sell)
-		if sellErr != nil {
-			log.Error(sellErr)
-		} else {
-			log.Infof("sold %d shares of %s with %s order", numShares, ticker, orderType)
-		}
+	buyCmd := cli.Command{
+		Name:    "buy",
+		Aliases: []string{"b"},
+		Usage:   "-t MSFT -s 10",
+		Flags: []cli.Flag{
+			&tickerFlag,
+			&sharesFlag,
+		},
+		Action: func(ctx *cli.Context) error {
+			ticker = strings.ToUpper(ticker)
+			buyErr := transactions.PlaceMarketOrder(rhClient, ticker, shares, robinhood.Buy)
+			if buyErr != nil {
+				log.Error(buyErr)
+			} else {
+				log.Infof("purchased %d shares of %s with %s order", shares, ticker, "market")
+			}
+			return buyErr
+		},
+	}
+
+	sellCmd := cli.Command{
+		Name:    "sell",
+		Usage:   "-t MSFT -s 10",
+		Flags: []cli.Flag{
+			&tickerFlag,
+			&sharesFlag,
+		},
+		Action: func(ctx *cli.Context) error {
+			ticker = strings.ToUpper(ticker)
+			sellErr := transactions.PlaceMarketOrder(rhClient, ticker, shares, robinhood.Sell)
+			if sellErr != nil {
+				log.Error(sellErr)
+			} else {
+				log.Infof("sold %d shares of %s with %s order", shares, ticker, "market")
+			}
+			return sellErr
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		&buyCmd,
+		&sellCmd,
+	}
+
+	appRunErr := app.Run(os.Args)
+	if appRunErr != nil {
+		log.Fatal(appRunErr)
 	}
 }
