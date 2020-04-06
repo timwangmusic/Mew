f263bbdfa2631b09c78c25bcf294ba897942845e
Tim Wang <weihewang2012@gmail.com>
2020-04-05 23:14:07 -0700

    refactor cli commands in commands pkg

--- mew.go
+++ mew.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"astuart.co/go-robinhood"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/urfave/cli/v2"
+	"github.com/weihesdlegend/Mew/commands"
 	"github.com/weihesdlegend/Mew/config"
-	"github.com/weihesdlegend/Mew/transactions"
 )
 
 // example input from CLI: mew buy -s 100 -t AAPL
@@ -42,67 +41,10 @@ func main() {
 
 	app := cli.NewApp()
 
-	var ticker string
-	var shares uint64
-
-	tickerFlag := cli.StringFlag{
-		Name:        "ticker",
-		Aliases:     []string{"t"},
-		Value:       "YANG",
-		Required:    false,
-		Destination: &ticker,
-	}
-
-	sharesFlag := cli.Uint64Flag{
-		Name:        "shares",
-		Aliases:     []string{"s"},
-		Value:       0,
-		Required:    false,
-		Destination: &shares,
-	}
-
-	buyCmd := cli.Command{
-		Name:    "buy",
-		Aliases: []string{"b"},
-		Usage:   "-t MSFT -s 10",
-		Flags: []cli.Flag{
-			&tickerFlag,
-			&sharesFlag,
-		},
-		Action: func(ctx *cli.Context) error {
-			ticker = strings.ToUpper(ticker)
-			buyErr := transactions.PlaceMarketOrder(rhClient, ticker, shares, robinhood.Buy)
-			if buyErr != nil {
-				log.Error(buyErr)
-			} else {
-				log.Infof("purchased %d shares of %s with %s order", shares, ticker, "market")
-			}
-			return buyErr
-		},
-	}
-
-	sellCmd := cli.Command{
-		Name:    "sell",
-		Usage:   "-t MSFT -s 10",
-		Flags: []cli.Flag{
-			&tickerFlag,
-			&sharesFlag,
-		},
-		Action: func(ctx *cli.Context) error {
-			ticker = strings.ToUpper(ticker)
-			sellErr := transactions.PlaceMarketOrder(rhClient, ticker, shares, robinhood.Sell)
-			if sellErr != nil {
-				log.Error(sellErr)
-			} else {
-				log.Infof("sold %d shares of %s with %s order", shares, ticker, "market")
-			}
-			return sellErr
-		},
-	}
-
+	commands.InitCommands(rhClient)
 	app.Commands = []*cli.Command{
-		&buyCmd,
-		&sellCmd,
+		&commands.MarketBuyCmd,
+		&commands.MarketSellCmd,
 	}
 
 	appRunErr := app.Run(os.Args)
