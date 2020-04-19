e7b4dbdfbfc6ebe1f04875dc032b311182201a19
Yang <coolcute@gmail.com>
2020-04-19 16:55:51 -0700

    Minor fix

--- commands/util.go
+++ commands/util.go
@@ -1,9 +1,20 @@
 package commands
 
 import (
+	"fmt"
 	"math"
+
+	"astuart.co/go-robinhood"
 )
 
 func round(x, unit float64) float64 {
 	return math.Round(x/unit) * unit
+}
+
+// About to place <market> <buy> for <10> shares of <QQQ> at <$200>
+func OrderToString(opts robinhood.OrderOpts, ins robinhood.Instrument) string {
+	ret := fmt.Sprintf("About to place %s %s for %d shares of %s at $%f",
+		opts.Type.String(), opts.Side.String(),
+		opts.Quantity, ins.OrderSymbol(), opts.Price)
+	return ret
 }
