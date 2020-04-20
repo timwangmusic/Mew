ef9551a2a2c3c52513a1846fe620049b04b65b41
Tim Wang <weihewang2012@gmail.com>
2020-04-19 21:06:11 -0700

    add limit buy test

---
+++ tests/limit_buy_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	"astuart.co/go-robinhood"
+	"flag"
+	"github.com/weihesdlegend/Mew/commands"
+	"testing"
+)
+
+var ticker = flag.String("ticker", "MSFT", "ticker")
+
+func TestPrepare(t *testing.T) {
+	mocker := new(RHClientMock)
+
+	quotes := []robinhood.Quote{
+		{LastTradePrice: 100.0,
+			LastExtendedHoursTradePrice: 105.0},
+	}
+	mocker.On("GetQuote", *ticker).Return(quotes, nil)
+
+	ins := &robinhood.Instrument{}
+	mocker.On("GetInstrument", *ticker).Return(ins, nil)
+
+	cmd := commands.LimitBuyCommand{
+		RhClient: mocker,
+	}
+
+	if err := cmd.Prepare(); err != nil {
+		t.Error(err)
+	}
+
+	mocker.AssertExpectations(t)
+}
