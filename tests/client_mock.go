9c4a30ae58ae41d26ffa230d66e5462d8deaddc4
Tim Wang <weihewang2012@gmail.com>
2020-04-22 14:47:54 -0700

    add test for LimitBuyCommand.Execute

--- tests/client_mock.go
+++ tests/client_mock.go
@@ -2,7 +2,7 @@ package tests
 
 import (
 	"astuart.co/go-robinhood"
-	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -11,17 +11,19 @@ type RHClientMock struct {
 }
 
 func (c *RHClientMock) GetQuote(ticker string) (quotes []robinhood.Quote, err error) {
-	fmt.Printf("method called with %s", ticker)
+	log.Infof("GetQuote called with %s", ticker)
 	args := c.Called(ticker)
 	return args.Get(0).([]robinhood.Quote), args.Error(1)
 }
 
 func (c *RHClientMock) GetInstrument(ticker string) (ins *robinhood.Instrument, err error) {
-	fmt.Printf("method called with %s", ticker)
+	log.Infof("GetInstrument called with %s", ticker)
 	args := c.Called(ticker)
 	return args.Get(0).(*robinhood.Instrument), args.Error(1)
 }
 
 func (c *RHClientMock) MakeOrder(ins *robinhood.Instrument, opts robinhood.OrderOpts) (orderOutput *robinhood.OrderOutput, err error) {
-	return
+	log.Infof("MakeOrder called")
+	args := c.Called(ins, opts)
+	return args.Get(0).(*robinhood.OrderOutput), args.Error(1)
 }
--- tests/limit_buy_test.go
+++ tests/limit_buy_test.go
@@ -2,31 +2,39 @@ package tests
 
 import (
 	"astuart.co/go-robinhood"
-	"flag"
 	"github.com/stretchr/testify/mock"
 	"github.com/weihesdlegend/Mew/commands"
 	"testing"
 )
 
-var ticker = flag.String("ticker", "MSFT", "ticker")
+var mocker = new(RHClientMock)
+var cmd = commands.LimitBuyCommand{
+	RhClient: mocker,
+	AmountLimit: 1000.0,
+	PercentLimit: 101.0,
+}
 
-func TestPrepare(t *testing.T) {
-	mocker := new(RHClientMock)
-
+func TestLimitBuy(t *testing.T) {
 	quotes := []robinhood.Quote{
 		{LastTradePrice: 100.0,
 			LastExtendedHoursTradePrice: 105.0},
 	}
-	mocker.On("GetQuote", mock.Anything).Return(quotes, nil)
+	mocker.On("GetQuote", mock.AnythingOfType("string")).Return(quotes, nil)
 
 	ins := &robinhood.Instrument{}
-	mocker.On("GetInstrument", mock.Anything).Return(ins, nil)
-
-	cmd := commands.LimitBuyCommand{
-		RhClient: mocker,
-	}
+	mocker.On("GetInstrument", mock.AnythingOfType("string")).Return(ins, nil)
 
 	if err := cmd.Prepare(); err != nil {
+		t.Error(err)
+	}
+
+	mocker.On("MakeOrder", mock.Anything, mock.Anything).Return(&robinhood.OrderOutput{ID: "33521"}, nil)
+
+	cmd.Opts = robinhood.OrderOpts{
+		Price: 105,
+	}
+
+	if err := cmd.Execute(); err != nil {
 		t.Error(err)
 	}
 
