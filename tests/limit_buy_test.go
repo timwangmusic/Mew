1c570776198084102d13d76662882f517dca5f12
Tim Wang <weihewang2012@gmail.com>
2020-04-22 17:12:06 -0700

    address comments

--- tests/limit_buy_test.go
+++ tests/limit_buy_test.go
@@ -11,13 +11,18 @@ var mocker = new(RHClientMock)
 var cmd = commands.LimitBuyCommand{
 	RhClient: mocker,
 	AmountLimit: 1000.0,
-	PercentLimit: 101.0,
+	PercentLimit: 99.0,
 }
 
+// test limit buy max $1000 worth of stock with limit of 99%
+// mock current price at 100.0
+// valid case
 func TestLimitBuy(t *testing.T) {
 	quotes := []robinhood.Quote{
-		{LastTradePrice: 100.0,
-			LastExtendedHoursTradePrice: 105.0},
+		{
+			LastTradePrice: 100.0,
+			LastExtendedHoursTradePrice: 100.0,
+		},
 	}
 	mocker.On("GetQuote", mock.AnythingOfType("string")).Return(quotes, nil)
 
@@ -26,6 +31,14 @@ func TestLimitBuy(t *testing.T) {
 
 	if err := cmd.Prepare(); err != nil {
 		t.Error(err)
+	}
+
+	if cmd.Opts.Price != 99.00 {
+		t.Errorf("expected price to be 103.95, got %.2f", cmd.Opts.Price)
+	}
+
+	if cmd.Opts.Quantity != 10 {
+		t.Errorf("expected quantity to be 10, got %d", cmd.Opts.Quantity)
 	}
 
 	mocker.On("MakeOrder", mock.Anything, mock.Anything).Return(&robinhood.OrderOutput{ID: "33521"}, nil)
